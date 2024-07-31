2d93d287a035ac7fd0c0687ccca0842830e8f538
Jocelyne Jones <gothling@jocely.net>
2024-07-31 16:10:52 +0100

    started on authors endpoint

--- authors.js
+++ authors.js
@@ -3,14 +3,39 @@ import { pool } from "./db/index.js";
 
 export async function getAuthors() {
   // Query the database and return all authors
+
+  // Define the SQL query to fetch all authors from the 'authors' table
+  const queryText = "SELECT * FROM authors";
+
+  // Use the pool object to send the query to the database
+  const result = await pool.query(queryText);
+
+  // The rows property of the result object contains the retrieved records
+  return result.rows;
 }
 
 export async function getAuthorById(id) {
   // Query the database and return the author with a matching id or null
+
+  // Define the SQL query to fetch the authors with the specified id from the 'authors' table
+  const queryText = "SELECT * FROM authors WHERE id = $1";
+
+  // Use the pool object to send the query to the database
+  // passing the id as a parameter to prevent SQL injection
+  const result = await pool.query(queryText, [id]);
+
+  // The rows property of the result object contains the retrieved records
+  // If a book with the specified id exists, it will be the first element in the rows array
+  // If no book exists with the specified id, the rows array will be empty
+  return result.rows[0] || null;
 }
 
 export async function createAuthor(author) {
   // Query the database to create an author and return the newly created author
+  const queryText = "INSERT INTO authors (first_name, last_name) VALUES ($1, $2) RETURNING id, first_name, last_name"
+  const result = await pool.query(queryText, [author.first_name, author.last_name]);
+  console.log(result);
+  return result.rows[0] || null;
 }
 
 export async function updateAuthorById(id, updates) {
